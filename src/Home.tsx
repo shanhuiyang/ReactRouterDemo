bb5a42a75ee398eff73c95f14d58b928ed1a1f08
Shan Huiyang <shanhuiyang@outlook.com>
2019-08-19 21:09:59 +0800

    init commit

---
+++ src/Home.tsx
@@ -0,0 +1,28 @@
+import React, { Fragment } from "react";
+import { match } from "react-router-native";
+import { Text, Header, Content, Title, Body } from "native-base";
+import TabNavigator from "./TabNavigator";
+import { Image } from "react-native";
+
+interface IProps {
+    match: match<any>
+};
+
+interface IStates {};
+
+export default class Home extends React.Component<IProps, IStates> {
+    render(): any {
+        return <Fragment>
+            <Header noLeft>
+                <Body>
+                    <Title>React Router Demo</Title>
+                </Body>
+            </Header>
+            <Content padder>
+                <Text> This is the home page of react router 4.0 demo.</Text>
+                <Image style={{ alignSelf: "center" }} source={require("../logo.png")}/>
+            </Content>
+            <TabNavigator/>
+        </Fragment>;
+    }
+}
---
+++ src/TopicDetail.tsx
@@ -0,0 +1,47 @@
+import React, { Fragment } from "react";
+import { Text, Header, Title, Body, Content, Button, Left, Right, Icon, H2, Card, CardItem } from "native-base";
+import { RouteComponentProps, Redirect } from "react-router-native";
+import Topic from "./Topic.d";
+
+interface IProps extends RouteComponentProps<any> {};
+
+interface IStates {};
+
+export default class TopicDetail extends React.Component<IProps, IStates> {
+    render(): any {
+        const topic: Topic | undefined = this.props.location.state;
+        // console.log("render the topic " + JSON.stringify(this.props));
+        if (topic) {
+            return <Fragment>
+                <Header >
+                    <Left>
+                        <Button transparent onPress={this.props.history.goBack}>
+                            <Icon name='arrow-back' />
+                        </Button>
+                    </Left>
+                    <Body>
+                        <Title>Topic Detail</Title>
+                    </Body>
+                    <Right>{/* nothing but counterbalance */}</Right>
+                </Header>
+                <Content padder>
+                    <Card>
+                        <CardItem header>
+                            <Text>{topic.title}</Text>
+                        </CardItem>
+                        <CardItem>
+                            <Body>
+                                <Text>
+                                    {topic.description}
+                                </Text>
+                            </Body>
+                        </CardItem>
+                    </Card>
+                </Content>
+                {/* No Footer in detail page */}
+            </Fragment>;
+        } else {
+            return <Redirect to="/home" />
+        }
+    }
+}

e31dc7a7ed0884b8fd59027272f463f61e1e8c30
Shan Huiyang <shanhuiyang@outlook.com>
2019-08-21 21:07:05 +0800

    bind physic back button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container } from "native-base";
-import { NativeRouter, Route } from "react-router-native";
+import { NativeRouter, Route, BackButton } from "react-router-native";
 import Topics from "./Topics";
 import Home from "./Home";
 import About from "./About";
@@ -8,6 +8,7 @@ import About from "./About";
 export default class App extends React.Component<any, any> {
     render() {
         return (<NativeRouter>
+            <BackButton />
             <Container>
                 <Route exact path="/" component={Home} />
                 <Route path="/topics" component={Topics} />

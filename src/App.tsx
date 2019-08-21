1c0b3c776cbfd51a7807d6aee87786cb3ba59d1b
Shan Huiyang <shanhuiyang@outlook.com>
2019-08-21 20:46:19 +0800

    remove back button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Footer, FooterTab } from "native-base";
-import { NativeRouter, Route, BackButton } from "react-router-native";
+import { NativeRouter, Route } from "react-router-native";
 import Topics from "./Topics";
 import Home from "./Home";
 import About from "./About";
@@ -9,7 +9,6 @@ import NavLink from "./NavLink";
 export default class App extends React.Component<any, any> {
     render() {
         return (<NativeRouter>
-            <BackButton />
             <Container>
                 <Route exact path="/" component={Home} />
                 <Route path="/topics" component={Topics} />

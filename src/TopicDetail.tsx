724ace6e977a930abde41b99554cc5feeb3ab927
Shan Huiyang <shanhuiyang@outlook.com>
2019-08-21 21:02:20 +0800

    add soft back button

--- src/TopicDetail.tsx
+++ src/TopicDetail.tsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import { Text, Header, Title, Body, Content } from "native-base";
+import { Text, Header, Title, Body, Content, Left, Button, Icon, Right } from "native-base";
 import { RouteComponentProps } from "react-router-native";
 
 interface IProps extends RouteComponentProps<any> {};
@@ -9,10 +9,16 @@ interface IStates {};
 export default class TopicDetail extends React.Component<IProps, IStates> {
     render(): any {
         return <Fragment>
-            <Header noLeft>
+            <Header >
+                <Left>
+                    <Button transparent onPress={this.props.history.goBack}>
+                        <Icon name='arrow-back' />
+                    </Button>
+                </Left>
                 <Body>
                     <Title>Topic Detail</Title>
                 </Body>
+                <Right>{/* nothing but counterbalance */}</Right>
             </Header>
             <Content padder>
                 <Text>TODO: a Topic detail card is to be implemented here.</Text>
